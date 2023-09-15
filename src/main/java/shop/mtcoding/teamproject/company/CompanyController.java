1204483b81ba7dc99c0b018e1360e43eeda8acfe
Yyyap <90502497+Yyyap@users.noreply.github.com>
2023-09-15 23:22:11 +0900

    수정요구

---
+++ src/main/java/shop/mtcoding/teamproject/company/CompanyController.java
@@ -0,0 +1,93 @@
+package shop.mtcoding.teamproject.company;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class CompanyController {
+    @Autowired
+    CompanyService companyService;
+
+    @Autowired
+    private HttpSession session;
+
+    @GetMapping("/companyJoinForm")
+    public String cjoinForm() {
+        return "/company/joinForm";
+    }
+
+    @GetMapping("/companyLoginForm")
+    public String cloginForm() {
+        return "/company/loginForm";
+    }
+
+    @GetMapping("/companyupdateForm")
+    public String cupdate(HttpServletRequest request) {
+        Company sessionUser = (Company) session.getAttribute("sessionCompany");
+        System.out.println("테스트1" + sessionUser.getCompanyId());
+        Company user = companyService.회원정보보기(sessionUser.getIndex());
+        request.setAttribute("company", user);
+        System.out.println("테스트2" + user.getCompanyId());
+
+        return "/company/updateForm";
+    }
+
+    @GetMapping("/companyDetailForm")
+    public String cdetailForm() {
+        return "/company/compinfoDetail";
+    }
+
+    @GetMapping("/company/compinfoUpdate")
+    public String compinfoUpdateForm() {
+        return "/company/compinfoUpdate";
+    }
+
+    @PostMapping("/company/compinfoUpdate")
+    public String compinfoUpdate(CompanyRequest.UpdatedetailDTO updatedetailDTO) {
+
+        Company sessionCompany = (Company) session.getAttribute("sessionCompany");
+        Company company = companyService.기업디테일수정(updatedetailDTO, sessionCompany.getIndex());
+        session.setAttribute("sessionCompany", company);
+        System.out.println("테스트1 : " + company.getCompanyName());
+
+        return "redirect:/companyinfoDetail";
+    }
+
+    @PostMapping("/companyJoin")
+    public String cjoin(CompanyRequest.compJoinDTO comjoinDTO) {
+        companyService.compjoin(comjoinDTO);
+        return "/";
+    }
+
+    @PostMapping("/companyLogin")
+    public void clogin(CompanyRequest.companyLoginDTO compLoginDTO, HttpServletResponse response) throws IOException {
+        Company sessioCompany = companyService.companylogin(compLoginDTO);
+
+        session.setAttribute("sessionCompany", sessioCompany);
+
+        response.sendRedirect("/");
+    }
+
+    @PostMapping("/companyupdateForm")
+    public String update(CompanyRequest.UpdateDTO updateDTO) {
+
+        Company sessionCompany = (Company) session.getAttribute("sessionCompany");
+        Company company = companyService.기업정보수정(updateDTO, sessionCompany.getIndex());
+        session.setAttribute("sessionCompany", company);
+        System.out.println("테스트1" + company.getEmail());
+        System.out.println("++++++++++++++비번바꿨다우");
+        return "redirect:/companyLoginForm";
+    }
+}
+// 수정시에는 영속성 컨텍스트 User오브젝트를 영속화시키고 영속화된 User 오브젝트를 수정
+// select해서 User오브젝트를 디비로 부터 가져오는 이유는 영속화를 하기 위해서
+// 영속화된 오브젝트를 변경하면 자동으로 디비에 업데이트문을 날려주거든요
+// 영속화된 persistance 객체의 변화가 감지되면 더티 체킹
